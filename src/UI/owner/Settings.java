d5d06cf1d67a15f8f32dedce2217f76bae075b4f
jaoks <amaruorihuela@gmail.com>
2019-05-20 20:52:06 -0500

    goes

--- src/UI/owner/Settings.java
+++ src/UI/owner/Settings.java
@@ -1,4 +1,43 @@
 package UI.owner;
 
 public class Settings {
+    private Object buttonPaymentConfirmation;
+    private Object buttonSupport;
+    private Object buttonTerms;
+    private Object buttonDeleteAccount;
+
+    public Settings() {
+    }
+
+    public Object getButtonPaymentConfirmation() {
+        return buttonPaymentConfirmation;
+    }
+
+    public void setButtonPaymentConfirmation(Object buttonPaymentConfirmation) {
+        this.buttonPaymentConfirmation = buttonPaymentConfirmation;
+    }
+
+    public Object getButtonSupport() {
+        return buttonSupport;
+    }
+
+    public void setButtonSupport(Object buttonSupport) {
+        this.buttonSupport = buttonSupport;
+    }
+
+    public Object getButtonTerms() {
+        return buttonTerms;
+    }
+
+    public void setButtonTerms(Object buttonTerms) {
+        this.buttonTerms = buttonTerms;
+    }
+
+    public Object getButtonDeleteAccount() {
+        return buttonDeleteAccount;
+    }
+
+    public void setButtonDeleteAccount(Object buttonDeleteAccount) {
+        this.buttonDeleteAccount = buttonDeleteAccount;
+    }
 }
